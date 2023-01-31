eb2230ef85fd5f828890af7fb45bd52978cd9d64
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-01-30 16:21:17 -0800

    Added a description to new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=500)
 
 with col2:
     st.title("Katie Janzen")
@@ -14,5 +14,9 @@ with col2:
     studying web development and application development at BCIT.  I'm currently 
     based in Vancouver, BC.
     """
-
     st.info(content)
+
+message = """
+Below are some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(message)
