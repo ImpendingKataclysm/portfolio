c2d6054a6f9cc7f3c0af3d74f70211eabe4d9077
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-02-04 10:16:17 -0800

    ...

--- send_email.py
+++ send_email.py
@@ -8,10 +8,13 @@ def send_email(message):
     host = "smtp.gmail.com"
     # standard port number
     port = 465
+
     username = "ktjanzen42@gmail.com"
     password = os.getenv("GOOGLE_APP_PASSWORD")
-    receiver = os.getenv("RECEIVER_EMAIL")
+
+    receiver = "ktjanzen42@gmail.com"
     context = ssl.create_default_context()
+
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
